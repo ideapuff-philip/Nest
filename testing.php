7a3c595efe86dce94582036d74b32de4d7dab4ac
ideapuff-philip <philip@ideapuff.com>
2013-08-27 16:28:59 -0400

    home page
    porduct catorgies

---
+++ testing.php
@@ -0,0 +1,70 @@
+<div class="category-products">
+    <h2></h2>
+    <ul class="products-grid one_column_5">
+        <li class="item">
+            <h3>Scented Candles</h3>
+            <div class="regular">
+                <a href=""><img src="{{media url="wysiwyg/scented-candles.jpg"}}" alt="" /></a>
+            </div>
+            <div style="text-align: center;">
+                <button class="button button_black" onclick="window.location = '{{store url=""}}'">
+                    <span>
+                        <span>Shop now</span>
+                    </span>
+                </button>
+            </div>
+        </li>
+        <li class="item">
+            <h3>Reed Diffusers</h3>
+            <div class="regular">
+                <a href=""><img src="{{media url="wysiwyg/reed-diffusers.jpg"}}" alt="" /></a>
+            </div>
+            <div style="text-align: center;">
+                <button class="button button_black" onclick="window.location = '{{store url=""}}'">
+                    <span>
+                        <span>Shop now</span>
+                    </span>
+                </button>
+            </div>
+        </li>
+        <li class="item ">
+            <h3>Bath &amp; Body</h3>
+            <div class="regular">
+                <a href=""><img src="{{media url="wysiwyg/bath-body.jpg"}}" alt="" /></a>
+            </div>
+            <div style="text-align: center;">
+                <button class="button button_black" onclick="window.location = '{{store url=""}}'">
+                    <span>
+                        <span>Shop now</span>
+                    </span>
+                </button>
+            </div>
+        </li>
+        <li class="item ">
+            <h3>Fine Fragrances</h3>
+            <div class="regular">
+                <a href=""><img src="{{media url="wysiwyg/fine-fragrances.jpg"}}" alt="" /></a>
+            </div>
+            <div style="text-align: center;">
+                <button class="button button_black" onclick="window.location = '{{store url=""}}'">
+                    <span>
+                        <span>Shop now</span>
+                    </span>
+                </button>
+            </div>
+        </li>
+        <li class="item ">
+            <h3>Gift Sets</h3>
+            <div class="regular">
+                <a href=""><img src="{{media url="wysiwyg/gift-sets.jpg"}}" alt="" /></a>
+            </div>
+            <div style="text-align: center;">
+                <button class="button button_black" onclick="window.location = '{{store url=""}}'">
+                    <span>
+                        <span>Shop now</span>
+                    </span>
+                </button>
+            </div>
+        </li>
+    </ul>
+</div>
